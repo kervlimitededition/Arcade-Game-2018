126f7d67362eba2450fd09f7dd575d8e5b613fa7
Kervintz Noel <kervintz84@hotmail.com>
2018-11-27 14:35:51 -0500

    winning condition works

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 //
-const playAgain = document.querySelector('.playAgain');
+let playAgain = document.querySelector('.playAgain');
 // const messageStatus = document.querySelector('.displayMessage');
 const ExitGame = document.querySelector('.exitGame');
 const modal = document.querySelector('.modal');
@@ -17,11 +17,20 @@ class Enemy {
         // a helper we've provided to easily load images
         this.sprite = 'images/enemy-bug.png';
         this.speed = speed;
-        // **Store the original x position (used to reset an enemy's position)
-        this.originalXPosition = xPosition;
-        // **Enemies cannot move until the game starts
-        this.canMove = true;
+        this.originalXPosition = xPosition; // **Store the original x position (used to reset an enemy's position)
+        this.canMove = true; // **Enemies cannot move until the game starts
     };
+
+    // Draw the enemy on the screen, required method for game
+    render(){
+        ctx.drawImage(Resources.get(this.sprite), this.xPosition, this.yPosition);
+    };
+
+    //let's reset the enemies position
+    resetPosition() {
+        this.xPosition = this.originalXPosition;
+    }
+
 }
 
 // Update the enemy's position, required method for game
@@ -31,31 +40,23 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     this.xPosition += this.speed * dt;
-    // Reset enemy position once it leaves the visible canvas
-    if (this.xPosition > 505) {
+    if (this.xPosition > 505) { // Reset enemy position once it leaves the visible canvas
         this.resetPosition();
     }
       
-      //check for crashes :)
-      if (!player.crashes) {
-          if(this.xPosition + 70 > player.xPosition &&        this.xPosition - 70 < player.xPosition &&
-              this.yPosition + 70 > player.yPosition && this.yPosition - 70 < player.yPosition){
-                  player.crashes = true;
-                  console.log('crashhhhh')
-                //   resetGame();
-              }
+    //check for crashes :)
+    if (!player.crashes) {
+        if(this.xPosition + 70 > player.xPosition && this.xPosition - 70 < player.xPosition &&
+            this.yPosition + 70 > player.yPosition && this.yPosition - 70 < player.yPosition){
+                player.crashes = true;
+                resetPlayer();
         }
+    }
+
+
 };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.xPosition, this.yPosition);
-    };
 
-//let's reset the enemies position
-Enemy.prototype.resetPosition = function() {
-    this.xPosition = this.originalXPosition;
-}
 
 // Now write your own player class
 class Player {
@@ -93,26 +94,44 @@ class Player {
         }
     }
 
-    // This class requires an update(), render() and
+    // This class requires an update(), render()
     update(dt) {
-        // console.log(this.yPosition);
-        // console.log(typeof this.yPosition);
-        // playerWin();
-        if(this.playerWon === true) {
-            playerWon = true;
-            
-        }
-    }
+        // this.playerWin();     
+        if(this.yPosition <= 69){
+            showMessage();
+            this.playerWon = true;
+            // modal.classList.toggle('show')
+        }  
+    };
+
+    playerWin() { // condition to check when player win
+        
+    };
 
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.xPosition, this.yPosition);
     };
 
+    //reset player position
+    resetPosition () {
+        this.xPosition = this.originalXPosition;
+        // this.hideMessage();
+        // allEnemies = [];
+    }
 }
 
+//give user an option to play the game again
+    playAgain.addEventListener('click', function() {
+        // resetGame();
+        window.location.reload();
+        player.resetPosition(); 
+    });
 
-
-
+//give user an option to exit the game
+    ExitGame.addEventListener('click', function() {
+        close();
+    });
+    
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 var allEnemies = [];
@@ -125,6 +144,24 @@ let littleBugs1 = new Enemy(10, 60, 100);
 let littleBugs2 = new Enemy(20, 143, 90);
 let littleBugs3 = new Enemy(30, 227, 90);
 allEnemies = [littleBugs1, littleBugs2, littleBugs3];
+
+//reset functions
+function resetPlayer() {
+    player = new Player(101, 81);
+}
+
+function resetGame() {
+    window.location.reload(true);
+
+}
+//adds and hide message when player win, start and restart the game.
+    function showMessage() {
+        modal.style.visibility = 'visible';
+    }
+
+// function hideMessage() {
+//     modal.style.visibility = 'hidden';
+// }
 
 
 // This listens for key presses and sends the keys to your
